dc32ee88ab192e88dd8f98c5dd0aa9cb83640b72
Venya Karri <v.karri0436@gmail.com>
2018-04-22 23:44:54 -0400

    Updated images

--- src/Integrated/R3/R3.ino
+++ src/Integrated/R3/R3.ino
@@ -1,30 +1,42 @@
 /*
 
- This program identifies metal with the help of
- Inductive Proximity Sensor and send signals to 
- microcontroller and notifies Motion sensor
- (Ultrasonic sensor) and a message "Please Recycle!" 
- on LCD display and finally trash bin lid will be
- opened for 4 seconds and then closes.
+ R3 program identifies Motion of the object with the help of 
+ Ultrasonic Sensor and also identifies metal with the help of  
+ Inductive Proximity Sensor. 
+ The serial signals will be sent to the microcontroller and notifies 
+ user wiht a message "Please Recycle!" on Liquid Crystal Display (LCD).
+ If the object is not recyclable item (non-metal in this scope) 
+ then Ultrasonic Sensor will send the signals to microcontroller 
+ and finally trash bin lid will be opened for 4 seconds and then closes.
+ 
+ All times the LCD will be displayed with the below information:
+ * Day of the Week
+ * Inside bin temperature in Fahrenheit
+ * Date
+ * Time
  
  We used Ananlog pins as Digital pins as some of 
- the digital pins that we used for LCD are needed for
+ the digital pins that we used for LCD were needed for the
  Ultrasonic sensor also, so we used Analog pins along with
  pinMode(.,.) as Digital pins, to avoid same pin connecting 
  to different components.
 
-  The circuit:
+  The circuit as explained below:
 
+ * Ultrasonic Senson as Motion Detector Input Device.
  * Trigger pin as Analog pin A3
  * Ultrasonic sensor power pin to Analog pin A4
  * echo pin to Analog pin A2
- * Servo pin to Analog pin A5
  * 
- * Inductive Proximity sensor as Metal Detector
+ * Inductive Proximity sensor as Metal Detector Input Device.
  * 
  * DS3231 as Real Time Clock (RTC) and also
- * acts as a Temperature Sensor.
+ * acts as a Temperature Sensor Input Device.
  * 
+ * DC Servo as Output Device
+ * Servo pin to Analog pin A5
+ * 
+ * Liquid Display (LCD) as Output Device.
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
  * LCD D4 pin to digital pin 5
@@ -38,13 +50,14 @@
  * ending to +5V and ground
  * wiper to LCD VO pin (pin 3)
  * 
- * Passive Buzzer for Digital Pin# 8
+ * Passive Buzzer as Output Device:
+ * For this buzzer Digital Pin# 8
  *    with 100 ohms resistor to 
  *    lessen the power to the buzzer
  *    
  *    
-
- Created on February 2018
+ 
+ @Created on February 2018
    by Venya Karri
       Srinidhi Akella
       Harini Devireddy
@@ -60,9 +73,9 @@
 #define close_angle 155   // close angle
 
 #define debug 0           // debug information (0 off, 1 on)
-//-------------------- SETTINGS ----------------------
 
-// --- WIRING ---
+
+//--------------- Motion Detector connections ----------------------
 /*
 #define trigPin 3
 #define echoPin 2
@@ -70,24 +83,18 @@
 #define MOSFETpin 6
 #define servoPin 5
 */
-
-//Ultrasonic Sensor connections
 #define echoPin A2
 #define trigPin A3
 #define sensorVCC A4
 #define servoPin A5
 #define MOSFETpin A6
-// --- WIRING ---
 
+//--------------- Include Libraries Code -------------------------
+#include <LiquidCrystal.h> //for LCD module
+#include <Servo.h>    //servo library,for motion detector module
+#include <DS3231.h> //for RTC and Temperature module
 
-// include the library code:
-#include <LiquidCrystal.h> // for LCD module
-
-//#include "LowPower.h" // sleep library
-#include <Servo.h>    // servo library,for motion detector module
-
-#include <DS3231.h> // for RTC module
-
+//--------------- Declare Variables start -------------------------
 DS3231  rtc(SDA, SCL);
 int tempC;
 int tempF;
@@ -111,7 +118,11 @@ byte distance;
 unsigned long open_timer;
 boolean open_flag;
 boolean test_flag = true;
+//--------------- Declare Variables end -------------------------
 
+
+//--------------- One Time Execution as start-up ----------------
+//Initializes and sets the initial values
 void setup() {
 
  // set up the LCD's number of columns and rows:
@@ -145,29 +156,7 @@ void setup() {
   
 }
 
-void open_cap() {
-  if (debug) Serial.println("open");
-  digitalWrite(MOSFETpin, 1);     // power up the servo - A6
-  delay(1);
-  servo.write(open_angle);        // open cap
-  delay(1000);                    // wait servo
-  digitalWrite(MOSFETpin, 0);     // disable servo - A6
-  open_flag = true;                  // remember that cap is open
-    Serial.println("Cap Opened");
-}
-void close_cap() {
-  if (debug) Serial.println("close");
-  digitalWrite(MOSFETpin, 1);     // power up the servo - A6
-  delay(1);
-  servo.write(close_angle);       // close cap
-  delay(1000);                    // wait servo
-  digitalWrite(MOSFETpin, 0);     // disable servo - A6
-  open_flag = false;                  // remember that cap is closed
-  Serial.println("Cap Closed");
-
-  
-}
-
+//---- Loops consecutively, allowing R3 program to change and respond ----------
 void loop() {
 
   displayTime();
@@ -180,8 +169,8 @@ void loop() {
     
   if(monitoring < 250)
   {
-  
-  
+      // The blelow logic is for Metal Detector
+
       unsigned long StartTime = millis();
   
        // set the cursor to column 0, line 1
@@ -220,34 +209,27 @@ void loop() {
           delay(500);
         }
         
-        //Serial.println("***********************");
-        //Serial.println("distance ---- "+ distance);
-        //Serial.println("maxH ---- "+ maxH);
-        //Serial.println(distance > 5 && distance < maxH);
-        //Serial.println("**********  test_flag *************" + test_flag);
         Serial.println("&&&&&&& open_flag value is &&&&&&&&&&&");
         Serial.println(open_flag);
         Serial.println("&&&&&&&$$$$$$$$$$$$$$$$&&&&&&&&&&&");
-       
-        if (distance >= 5 && distance <= maxH) { // if distance fit the range of 5cm to 15cm
-        //  if (distance > 5) {
+        
+        //if distance fit the range of 5cm to 15cm
+        if (distance >= 5 && distance <= maxH) { 
           Serial.println("testing.... IF...");
-          open_timer = 0;                     // reset timer
-          if (!open_flag) {                   // if cap is closed
+          open_timer = 0;                     //Reset timer
+          if (!open_flag) {                   //If cap is closed
             Serial.println("testing....inside IF -- !open_flag ...");
-            tone(piezoPin, 7000, 2500); // Pin Number, Frequency in Hz, duration in milli seconds
+            tone(piezoPin, 7000, 2500);       //Pin Number, Frequency in Hz, 
+                                              //duration in milli seconds
             open_cap();
-            //delay(1000);
-            //delay(1000);
-            //delay(1000);
-            //delay(1000);
           }
         } else {
           Serial.println("testing....ELSE...");
           if (open_flag) {                    // if cap is open
             Serial.println("testing....inside ELSE -- open_flag ...");
-            open_timer++;                     // timer increments by 1 sec each time (open_timer = open_timer + 1)
-            if (open_timer > cap_time) {      // if passed more than cap_time after cap was opened
+            open_timer++;                     //Timer increments by 1 sec each time 
+                                              // (open_timer = open_timer + 1)
+            if (open_timer > cap_time) {      //if cap_time exceeds after cap was opened
               close_cap();
             }
           }
@@ -255,14 +237,34 @@ void loop() {
     
   }
   
-  /*Serial.println("monitoring value = " + monitoring);
-  Serial.println("testing....");*/
   Serial.println(var);
   var = var + 5; 
   delay(1000);
 
-
 } //end of loop
+
+
+void open_cap() {
+  if (debug) Serial.println("open");
+  digitalWrite(MOSFETpin, 1);     // power up the servo - A6
+  delay(1);
+  servo.write(open_angle);        // open cap
+  delay(1000);                    // wait servo
+  digitalWrite(MOSFETpin, 0);     // disable servo - A6
+  open_flag = true;                  // remember that cap is open
+  Serial.println("Cap Opened");
+}
+void close_cap() {
+  if (debug) Serial.println("close");
+  digitalWrite(MOSFETpin, 1);     // power up the servo - A6
+  delay(1);
+  servo.write(close_angle);       // close cap
+  delay(1000);                    // wait servo
+  digitalWrite(MOSFETpin, 0);     // disable servo - A6
+  open_flag = false;                  // remember that cap is closed
+  Serial.println("Cap Closed");
+}
+
 
 // distance measuring custom function using pulseIn and below formula
 // (duration / 2) / 29.1 ---> OR (duration / 2) * 0.0344;
@@ -277,51 +279,40 @@ byte measure() {
 }
 
 
-//displays time on the LCD
+//displays DOW, Date and Time on the LCD
 byte displayTime(){
-  
   lcd.setCursor(0, 0);
   lcd.print("                      ");
   lcd.setCursor(0, 1);
   lcd.print("                      ");
   lcd.setCursor(0, 0);
-
   lcd.print(rtc.getDOWStr());
   lcd.setCursor(0, 1);
   lcd.print(rtc.getDateStr());
   lcd.print(" ");
   lcd.print(rtc.getTimeStr());
 
-  
   // Send Day-of-Week
   Serial.print(rtc.getDOWStr());
   Serial.print(" ");
-  
   // Send date
   Serial.print(rtc.getDateStr());
   Serial.print(" -- ");
-
   // Send time
   Serial.println(rtc.getTimeStr());
-
   //to display temperature
   displayTemp(); 
-  
   // Wait one second before repeating :)
-  delay (1000);
-  
+  delay (1000);  
 }
 
-//displays the temperature inside the trashcan
+//displays the Temperature inside the trashcan in Fahrenheit
  byte displayTemp(){ 
-
   tempC = rtc.getTemp();
   tempF = (tempC * 1.8) + 32.0; // Convert C to F
   lcd.setCursor(12, 0);
-  
   lcd.print(tempF);
   lcd.print("F");
-  
 }
 
 
